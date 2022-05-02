f45cd3273d8d9bd4520118e84b6f6817a60aeb1c
Jonathan Simson <jonathan@tktk.ee>
2022-05-02 18:58:21 +0300

    new

---
+++ UserNameData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada oma eesnime
+            //programm kuvab kasutaja eesnime pikkust
+            //programm kuvab kasutaja eesnime esimest tähte
+            //programm kuvab kasutaja eesnime tagurpidi
+            //main meetodis ei tohi olla rohkem, kui 3 rida koodi
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string UserName = Console.ReadLine();
+
+            GetUserNameData(UserName);
+
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene sümbol on {userInput[0]}");
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //programm küsib kasutajal sisestada oma eesnime
+            //programm küsib kasutajal sisestada number 1-3
+            //kui sisestab 1, kuvatakse kasutaja eesnimi tagurpidi
+            //kui sisestab 2, kuvatakse kasutaja eesnime esimest tähte
+            //kui sisestab 3, kuvatakse kasutaja eesnime pikkus
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Sisesta arv 1-3:");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (UserNumber == 1)
+            {
+                UserNumber1(UserName);
+            }
+            else if (UserNumber == 2)
+            {
+                UserNumber2(UserName);
+            }
+            else if (UserNumber == 3)
+            {
+                UserNumber3(UserName);
+            }
+            else
+            {
+                Console.WriteLine("Sisestasid vale numbri.");
+            }
+        }
+
+        public static void UserNumber1(string UserInput)
+        {
+            for (int i = UserInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(UserInput[i]);
+            }
+        }
+        public static void UserNumber2(string UserInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene sümbol on {UserInput[0]}");
+        }
+        public static void UserNumber3(string UserInput)
+        {
+            Console.WriteLine($"Sinu nimi on {UserInput.Length} sümbolit pikk.");
+        }
+    }
+}
